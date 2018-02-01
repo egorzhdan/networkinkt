64fe38631d3998dfc71cfa9806e333cb48b42268
Egor Zhdan <egor.zhdan@gmail.com>
2018-02-01 20:37:36 +0300

    Add more request types

--- networkinkt-common/src/main/kotlin/com/egorzh/networkinkt/HTTP.kt
+++ networkinkt-common/src/main/kotlin/com/egorzh/networkinkt/HTTP.kt
@@ -5,5 +5,9 @@ package com.egorzh.networkinkt
  */
 object HTTP {
     fun get(url: String, headers: Map<String, String> = emptyMap()) = HTTPRequest(url, "GET", null, headers)
+    fun head(url: String, headers: Map<String, String> = emptyMap()) = HTTPRequest(url, "HEAD", null, headers)
     fun post(url: String, headers: Map<String, String> = emptyMap(), body: String? = null) = HTTPRequest(url, "POST", body, headers)
+    fun delete(url: String, headers: Map<String, String> = emptyMap(), body: String? = null) = HTTPRequest(url, "DELETE", body, headers)
+    fun patch(url: String, headers: Map<String, String> = emptyMap(), body: String? = null) = HTTPRequest(url, "PATCH", body, headers)
+    fun put(url: String, headers: Map<String, String> = emptyMap(), body: String? = null) = HTTPRequest(url, "PUT", body, headers)
 }
